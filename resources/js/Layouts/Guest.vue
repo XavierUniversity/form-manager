bdd57addfb985dd1582d062ad860c987e00c0a37
Greg McMullen <mcmullen.greg@gmail.com>
2021-12-09 10:08:30 -0500

    Update Guest.vue
    Alter the logo, and change out some of the defaults from LaravelMix's styles.

--- resources/js/Layouts/Guest.vue
+++ resources/js/Layouts/Guest.vue
@@ -2,11 +2,12 @@
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
         <div>
             <Link href="/">
-                <BreezeApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
+                <BreezeApplicationLogo />
+                <h1 class="text-xl pt-2 font-bold">Form Manager</h1>
             </Link>
         </div>
 
-        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4">
             <slot />
         </div>
     </div>
