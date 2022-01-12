99f5865bd7e988b8aa5a475d36f1fc4dd6e0e3f9
Greg McMullen <mcmullen.greg@gmail.com>
2022-01-11 23:14:50 -0500

    Updates to the basic layouts (auth and general)

--- resources/js/Layouts/Guest.vue
+++ resources/js/Layouts/Guest.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+      <theme-switcher />
         <div>
             <Link href="/">
                 <BreezeApplicationLogo />
                 <h1 class="text-xl pt-2 font-bold">Form Manager</h1>
             </Link>
         </div>
-
         <div class="w-full sm:max-w-md mt-6 px-6 py-4">
             <slot />
         </div>
@@ -15,6 +15,7 @@
 
 <script>
 import BreezeApplicationLogo from '@/Components/ApplicationLogo.vue'
+import ThemeSwitcher from '@/Components/ThemeSwitcher.vue'
 import { Link } from '@inertiajs/inertia-vue3';
 
 export default {
