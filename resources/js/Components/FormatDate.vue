45618393113e4ad912671efc5187e684d7ab4b80
Greg McMullen <mcmullen.greg@gmail.com>
2022-01-21 15:53:42 -0500

    Create FormatDate.vue
    Component useful for modifying dates on the fly -- creates a nice locale based layout for the created/updated dates.

---
+++ resources/js/Components/FormatDate.vue
@@ -0,0 +1,21 @@
+<template>
+    {{ formatDate(date) }}
+</template>
+
+<script>
+export default {
+    props: {
+        date: {
+            type: String,
+            default: false,
+            required: true
+        },
+    },
+    methods: {
+        formatDate(dateString) {
+            const date = new Date(dateString);
+            return new Intl.DateTimeFormat('default', { month: 'numeric', day: 'numeric', year: 'numeric', hour: 'numeric', minute: '2-digit'}).format(date);
+        }
+    },
+}
+</script>
