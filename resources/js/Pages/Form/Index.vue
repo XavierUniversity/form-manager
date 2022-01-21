def67cc71b7d1127b822b8a24450baa1c8d84046
Greg McMullen <mcmullen.greg@gmail.com>
2022-01-21 15:55:13 -0500

    Update Index.vue
    * Update the table to be a bit more mobile friendly
    * Add FormatDate
    * Add link to the Show view
    * Add the DeleteForm component in this view

--- resources/js/Pages/Form/Index.vue
+++ resources/js/Pages/Form/Index.vue
@@ -9,7 +9,8 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
-                        <table class="table-auto">
+                        <div class="overflow-auto lg:overflow:visible">
+                        <table class="table-auto w-screen md:w-full">
                             <thead>
                                 <tr class="border-b border-t bg-gray-100 border-gray-200">
                                     <th class="py-2 px-2">Form ID</th>
@@ -20,14 +21,29 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr v-for="form in forms.data" class="border-b border-gray-200" v-bind="form.id">
+                                <tr v-for="form in forms.data" class="border-b border-gray-200" v-bind:key="form.id">
                                     <td class="py-2 px-2">{{ form.id }}</td>
                                     <td class="py-2 px-2">{{ form.name }}</td>
-                                    <td class="py-2 px-2">{{ formatDate(form.created_at) }}</td>
+                                    <td class="py-2 px-2"><FormatDate :date="form.created_at" /></td>
                                     <td class="py-2 px-2">{{ form.user_id}}</td>
+                                    <td class="p-2">
+                                        <Link class="p-2 mr-2 border-2 inline-block rounded hover:bg-gray-200" :href="route('forms.edit', form.id)">
+                                            <PencilIcon class="h-5 w-5 text-blue-500" />
+                                            <span class="sr-only">Edit Form: {{ form.name }}</span>
+                                        </Link>
+                                        <Link class="p-2 mr-2 border-2 inline-block rounded hover:bg-gray-200" :href="route('forms.show', form.id)">
+                                            <EyeIcon class="h-5 w-5 text-blue-500" />
+                                            <span class="sr-only">View Form Details: {{ form.name }}</span>
+                                        </Link>
+                                        <DeleteForm :form="form" class="p-2 border-2 inline-block rounded hover:bg-gray-200">
+                                            <TrashIcon class="h-5 w-5 text-blue-500" />
+                                            <span class="sr-only">Delete Form: {{ form.name }}</span>
+                                        </DeleteForm>
+                                    </td>
                                 </tr>
                             </tbody>
                         </table>
+                        </div>
                         <div class="flex flex-row justify-between pt-3">
                             <div class="basis-1/2">
                             <Link v-if="forms.prev_page_url !== null" :href="forms.prev_page_url" class="underline text-sm text-gray-600 hover:text-gray-900">
@@ -48,31 +64,26 @@
 </template>
 <script>
 import BreezeAuthenticatedLayout from "@/Layouts/Authenticated.vue";
+import FormatDate from "@/Components/FormatDate.vue";
+import DeleteForm from "@/Components/DeleteForm.vue";
+import { EyeIcon, PencilIcon, TrashIcon } from "@heroicons/vue/outline";
 import { Head, Link } from "@inertiajs/inertia-vue3";
 export default {
     components: {
         BreezeAuthenticatedLayout,
+        FormatDate,
+        EyeIcon,
+        PencilIcon,
+        TrashIcon,
+        DeleteForm,
         Head,
         Link,
     },
     props: {
         forms: Object,
     },
-    methods: {
-        formatDate(dateString) {
-            const date = new Date(dateString);
-            return new Intl.DateTimeFormat('default', { month: 'numeric', day: 'numeric', year: 'numeric', hour: 'numeric', minute: '2-digit'}).format(date);
-        }
-    },
 }
 </script>
 
 <style lang="scss" scoped>
-    table{
-        width: 100%;
-
-        thead{
-            text-align: left;
-        }
-    }
 </style>
