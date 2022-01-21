8d25f0a8bde39fc311e792c561a473685d98c703
Greg McMullen <mcmullen.greg@gmail.com>
2022-01-21 15:52:20 -0500

    Create DeleteForm.vue
    Component for the delete functionality on forms.

---
+++ resources/js/Components/DeleteForm.vue
@@ -0,0 +1,33 @@
+<template>
+    <Link @click="destroy(form)" :class="class">
+        <slot />
+    </Link>
+</template>
+
+<script>
+import { Link } from "@inertiajs/inertia-vue3";
+import { TrashIcon } from "@heroicons/vue/solid";
+export default {
+    components: {
+        Link,
+        TrashIcon,
+    },
+    props: {
+        form: {
+            type: Object,
+            required: true,
+        },
+        class:  {
+            type: String,
+            required: false
+        },
+    },
+    methods: {
+        destroy(form) {
+            if ( confirm(`Confirmation: Delete the form ${form.name}`) )
+                this.$inertia.delete(route("forms.destroy", form.id));
+        }
+    }
+}
+
+</script>
