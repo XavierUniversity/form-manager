e8c78b4ed4226292f0bf0945eb88179a5ac2871d
Greg McMullen <mcmullen.greg@gmail.com>
2022-04-06 17:04:42 -0400

    Delete Field item

---
+++ resources/js/Components/DeleteField.vue
@@ -0,0 +1,32 @@
+<template>
+    <Link @click="destroy(field)" :class="class">
+        <slot />
+    </Link>
+</template>
+
+<script>
+import { Link } from "@inertiajs/inertia-vue3";
+import { TrashIcon } from "@heroicons/vue/solid";
+export default {
+    components: {
+        Link,
+        TrashIcon,
+    },
+    props: {
+        field: {
+            type: Object,
+            required: true,
+        },
+        class:  {
+            type: String,
+            required: false
+        },
+    },
+    methods: {
+        destroy(field) {
+            if ( confirm(`Confirmation: Delete the field ${field.name}`) )
+                this.$inertia.delete(route("form_fields.destroy", field.id));
+        }
+    }
+}
+</script>
